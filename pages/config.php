700e603677fcde03d51d296b2774e965c2a8332e
schuer <ds@decaf.de>
2019-05-22 10:19:35 +0200

    overhaul config page

--- pages/config.php
+++ pages/config.php
@@ -1,68 +1,78 @@
 <?php
-	$content = '';
-	
-	if (rex_post('config-submit', 'boolean')) {
-		$this->setConfig(rex_post('config', [
-			['optimization_tool', 'string'],
-			['tinify_key', 'string'],
-		]));
+    /** @var rex_addon $this */
 
-		$content .= rex_view::info($this->i18n('config_saved'));
-	}
+    $csrfToken = rex_csrf_token::factory('minify_images');
 
-	$content .= '<div class="rex-form">';
-	$content .= '  <form action="'.rex_url::currentBackendPage().'" method="post">';
-	$content .= '    <fieldset>';
-	
-	$formElements = [];
-	
-	//Start - optimization_tool
-		$n = [];
-		$n['label'] = '<label for="minify-config-optimization_tool">'.$this->i18n('config_optimization_tool').'</label>';
-		$select = '';
-		$select .= '<select id="minify-config-optimization_tool" name="config[optimization_tool]">';
-		$select .= '  <option value="none">---</option>';
-		$select .= '  <option value="ImageOptimizer" '.(($this->getConfig('optimization_tool') == 'ImageOptimizer') ? 'selected="selected"' : '').'>ImageOptimizer (pngquant / optipng / pngcrush / pngout / advpng / jpegtran / jpegoptim / gifsicle)</option>';
-		$select .= '  <option value="Tinify" '.(($this->getConfig('optimization_tool') == 'Tinify') ? 'selected="selected"' : '').'>Tinify (tinyJPEG / tinyPNG)</option>';
-		$select .= '  <option value="Imagick" '.(($this->getConfig('optimization_tool') == 'Imagick') ? 'selected="selected"' : '').'>Imagemagick</option>';
-		$select .= '</select>';
-		
-		$n['field'] = $select;
-		$formElements[] = $n;
-	//End - optimization_tool
-	
-	//Start - tinify_key
-		$n = [];
-		$n['label'] = '<label for="minify-config-tinify_key">'.$this->i18n('config_tinify_key').'</label>';
-		$n['field'] = '<input type="text" id="minify-config-tinify_key" name="config[tinify_key]" value="'.$this->getConfig('tinify_key').'"/>';
-		$formElements[] = $n;
-	//End - tinify_key
-	
-	$fragment = new rex_fragment();
-	$fragment->setVar('elements', $formElements, false);
-	$content .= $fragment->parse('core/form/form.php');
-	
-	$content .= '    </fieldset>';
-	
-	$content .= '    <fieldset class="rex-form-action">';
-	
-	$formElements = [];
-	
-	$n = [];
-	$n['field'] = '<input type="submit" name="config-submit" value="'.$this->i18n('config_action_save').'" '.rex::getAccesskey($this->i18n('config_action_save'), 'save').'>';
-	$formElements[] = $n;
-	
-	$fragment = new rex_fragment();
-	$fragment->setVar('elements', $formElements, false);
-	$content .= $fragment->parse('core/form/submit.php');
-	
-	$content .= '    </fieldset>';
-	$content .= '  </form>';
-	$content .= '</div>';
-	
+    if (rex_post('config-submit', 'boolean')) {
+        if (!$csrfToken->isValid()) {
+
+            echo rex_view::error(rex_i18n::msg('csrf_token_invalid'));
+        }
+        else {
+
+            $this->setConfig(rex_post('config', [
+                ['optimization_tool', 'string'],
+                ['tinify_key', 'string'],
+            ]));
+            echo rex_view::success($this->i18n('config_saved'));
+        }
+    }
+
+
+    $content = '<fieldset>';
+
+    $formElements = [];
+
+	// optimization_tool
+    $sel_optimization_tool = new rex_select();
+    $sel_optimization_tool->setid('minify-config-optimization_tool');
+    $sel_optimization_tool->setName('config[optimization_tool]');
+    $sel_optimization_tool->setSize(1);
+    $sel_optimization_tool->setAttribute('class', 'form-control selectpicker');
+    $sel_optimization_tool->setSelected($this->getConfig('optimization_tool'));
+    $sel_optimization_tool->addOption('', 'none');
+    $sel_optimization_tool->addOption('ImageOptimizer (pngquant / optipng / pngcrush / pngout / advpng / jpegtran / jpegoptim / gifsicle)', 'ImageOptimizer');
+    $sel_optimization_tool->addOption('Tinify (tinyJPEG / tinyPNG)', 'Tinify');
+    $sel_optimization_tool->addOption('Imagemagick', 'Imagick');
+
+    $n = [];
+    $n['label'] = '<label for="minify-config-optimization_tool">' . $this->i18n('config_optimization_tool') . '</label>';
+    $n['field'] = $sel_optimization_tool->get();
+    $formElements[] = $n;
+
+    // tinify_key
+    $n = [];
+    $n['label'] = '<label for="minify-config-tinify_key">'.$this->i18n('config_tinify_key').'</label>';
+    $n['field'] = '<input class="form-control" type="text" id="minify-config-tinify_key" name="config[tinify_key]" value="'.$this->getConfig('tinify_key').'"/>';
+    $n['note'] = $this->i18n('minify_images_config_tinify_key_note');
+    $formElements[] = $n;
+
+    $fragment = new rex_fragment();
+    $fragment->setVar('elements', $formElements, false);
+    $content .= $fragment->parse('core/form/container.php');
+
+
+    $formElements = [];
+    $n = [];
+    $n['field'] = '<button class="btn btn-save rex-form-aligned" type="submit" name="config-submit" value="1">' . $this->i18n('config_save') . '</button>';
+    $formElements[] = $n;
+
+    $fragment = new rex_fragment();
+    $fragment->setVar('flush', true);
+    $fragment->setVar('elements', $formElements, false);
+    $buttons = $fragment->parse('core/form/submit.php');
+
+
 	$fragment = new rex_fragment();
 	$fragment->setVar('class', 'edit');
 	$fragment->setVar('title', $this->i18n('config'));
-	$fragment->setVar('body', $content, false);
-	echo $fragment->parse('core/page/section.php');
+    $fragment->setVar('body', $content, false);
+    $fragment->setVar('buttons', $buttons, false);
+    $content = $fragment->parse('core/page/section.php');
+
+    echo '
+        <form action="' . rex_url::currentBackendPage() . '" method="post">
+            ' . $csrfToken->getHiddenField() . '
+            ' . $content . '
+        </form>';
 ?>
